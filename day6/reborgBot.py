8eff35f868ecaab87e08e570591327834b1e8e03
sam obadan <obadansam@gmail.com>
2024-06-29 09:50:09 +0100

    all use cases resolved for day 6

--- day6/reborgBot.py
+++ day6/reborgBot.py
@@ -7,13 +7,22 @@ def turn_right():
 # front_is_clear() or wall_in_front(),
 # right_is_clear() or wall_on_right(), and at_goal().
 
-while(not at_goal()):
-    if right_is_clear():
+def wall_on_left():
+    turn_left()
+    if wall_in_front():
         turn_right()
+        return True
+    turn_right()
+    return False
+
+while(not at_goal()):
+    if not wall_on_left() and not wall_on_right() and front_is_clear():
         move()
-    elif front_is_clear():
-        move()
-    elif wall_in_front() and wall_on_right():
-        turn_left()
     elif right_is_clear():
         turn_right()
+        move()
+    elif wall_on_right() and wall_in_front():
+        turn_left()
+    elif front_is_clear():
+        move()
+    
