55a4f0778e0013cea605fa50c4ffbcee939ab797
sam obadan <obadansam@gmail.com>
2024-07-05 16:31:40 +0100

    day 18 completed

---
+++ day18/main_color.py.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# pip install colorgram.py
+
+
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    rgb_colors.append(color.rgb)
+
+print(rgb_colors)
